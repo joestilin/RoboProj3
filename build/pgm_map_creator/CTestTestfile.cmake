8ddd16da4b02b962cf240483373e510978b90067
robond <jstilin@gmail.com>
2020-10-25 10:22:31 -0700

    Modified directory structure

--- build/pgm_map_creator/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/robond/workspace/RoboProj3/src/pgm_map_creator
-# Build directory: /home/robond/workspace/RoboProj3/build/pgm_map_creator
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs(msgs)
